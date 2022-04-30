59f2e952504ed3795be92a8fb977a49fec2e203d
Chirag Kushwaha <chirag@Chirags-MacBook-Air.local>
2022-04-30 19:09:25 +0530

    final commit

--- app/routes/films/index.tsx
+++ app/routes/films/index.tsx
@@ -5,12 +5,12 @@ import {
   LoaderFunction,
   MetaFunction,
   useLoaderData,
-} from 'remix';
-import { Film, getFilms } from '~/api/films';
+} from "remix";
+import { Film, getFilms } from "~/api/films";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
-  const title = url.searchParams.get('title');
+  const title = url.searchParams.get("title");
   return getFilms(title);
 };
 
@@ -18,11 +18,11 @@ export default function FilmsIndex() {
   const films = useLoaderData<Film[]>();
   return (
     <div className="p-16 font-sans">
-      <h1 className="text-5xl font-bold text-center">Studio Ghibli Films</h1>
+      <h1 className="text-5xl font-bold text-center">Anime Movies</h1>
 
       <Form reloadDocument method="get" className="py-5">
         <label className="font-bold">
-          Search{' '}
+          Search{" "}
           <input
             type="text"
             name="title"
@@ -40,7 +40,7 @@ export default function FilmsIndex() {
       </Form>
 
       <div className="grid grid-cols-4 gap-4">
-        {films.map((film) => (
+        {films.map(film => (
           <Link
             title={film.title}
             key={film.id}
@@ -58,9 +58,12 @@ export default function FilmsIndex() {
 }
 
 export const meta: MetaFunction = () => {
-  return { title: 'Films | Studio Ghibli', description: 'List of films' };
+  return {
+    title: "Films | Anime movies",
+    description: "These movies will make you cry",
+  };
 };
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: 'styles' }];
+  return [{ rel: "stylesheet", href: "styles" }];
 };
